593f2e3e615c3c20444ffce145f6098655b92816
Balajiganapathi S <balajiganapathi.s@gmail.com>
2020-01-08 23:33:00 +0530

    Fetch problem from content view service

--- src/store/modules/problem.js
+++ src/store/modules/problem.js
@@ -2,6 +2,8 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import site from "codedrills_proto/io/codedrills/proto/site/content_view_service_grpc_web_pb";
+var site_proto = proto.io.codedrills.proto.site;
+var content_proto = proto.io.codedrills.proto.content;
 
 const siteService = new site.ContentViewServicePromiseClient(
   "http://site.staging.codedrills.io:80",
@@ -10,10 +12,6 @@ const siteService = new site.ContentViewServicePromiseClient(
 );
 
 const state = {
-  userContents: null /* All user contents */,
-  current: null /* Content being currently edited */,
-  errorCreateContent: null,
-  errorUpdateContent: null,
   title: "Counting Valleys",
   tags: ["dp", "graphs", "category"],
   companies: ["Google", "Facebook", "Microsoft"],
@@ -59,78 +57,32 @@ const actions = {
     // }
     // return tokenPromise;
   },
-  async fetchProblems({ commit, state, getters, dispatch }) {
-    console.log("TRYING TO FETCH");
-    return dispatch("ensureToken").then(__ => {
-      var listContentRequest = new proto.io.codedrills.proto.site.ListContentRequest();
-      listContentRequest.setContentType(
-        proto.io.codedrills.proto.content.ContentType.PROBLEM
-      );
-      return siteService
-        .listContent(listContentRequest, { "auth-token": getters.userToken })
-        .then(res => {
-          console.log("SUCCESS", res);
-        })
-        .catch(err => {
-          console.log("ERROR", err);
-        });
-    });
-  },
   async fetchProblem({ commit, state, getters, dispatch }) {
+    var url = "two-sum"
     console.log("TRYING TO FETCH");
     console.log(commit);
     return dispatch("ensureToken").then(__ => {
-      var listContentRequest = new proto.io.codedrills.proto.site.ListContentRequest();
-      listContentRequest.setContentType(
-        proto.io.codedrills.proto.content.ContentType.PROBLEM
-      );
-      return siteService
-        .listContent(listContentRequest, { "auth-token": getters.userToken })
+      var getContentRequest = new site_proto.GetContentRequest();
+      var contentId = new content_proto.ContentId();
+      var contentAddress = new content_proto.ContentAddress();
+      contentAddress.setContentType(proto.io.codedrills.proto.content.ContentType.PROBLEM);
+      contentAddress.setUrl(url);
+      contentId.setContentAddress(contentAddress)
+      getContentRequest.setContentId(contentId);
+
+      return siteService.getContent(getContentRequest, { "auth-token": getters.userToken })
         .then(res => {
-          console.log("SUCCESS", res);
+          console.log("SUCCESS", res.toObject());
+          commit("setProblem", res.toObject().contentView);
         })
         .catch(err => {
           console.log("ERROR", err);
         });
     });
-  },
-  async fetchCurrent({ commit, state, getters, dispatch }, data) {
-    var req = new proto.ai.codedrills.ContentRequest();
-    var typeAndUrl = new proto.ai.codedrills.TypeAndUrl();
-    typeAndUrl.setType(
-      proto.ai.codedrills.ContentType[data.type.toUpperCase()]
-    );
-    typeAndUrl.setUrl(data.url);
-    req.setTypeAndUrl(typeAndUrl);
-    return dispatch("ensureToken").then(_ => {
-      orbService.get(req, { "auth-token": getters.userToken }).then(content => {
-        console.log("Got content", content.toObject());
-        commit("setCurrent", content.toObject());
-      });
-    });
-  },
-  async updateContent({ commit, state }, content) {
-    commit("setCurrent", content);
-  },
-  async createContent({ commit, state, dispatch, getters }, contentData) {
-    var createContent = new proto.ai.codedrills.CreateContent();
-    createContent.setTitle(contentData.title);
-    createContent.setType(proto.ai.codedrills.ContentType[contentData.type]);
-    return orbService
-      .create(createContent, { "auth-token": getters.userToken })
-      .then(res => {
-        return dispatch("fetchContents");
-      });
   }
 };
 
 const getters = {
-  userContents: state => {
-    return state.userContents;
-  },
-  current: state => {
-    return state.current;
-  },
   userToken: function(state, getters, rootState, rootGetters) {
     console.log("rootGetters", rootGetters);
     return rootGetters["user/userToken"];
@@ -141,11 +93,28 @@ const getters = {
 };
 
 const mutations = {
-  setUserContents(state, contents) {
-    state.userContents = contents;
-  },
-  setCurrent(state, current) {
-    state.current = current;
+  setProblem(state, content) {
+    state.title = content.title;
+    var problemView = content.dataView.problemView;
+    var statement = problemView.statementView; // TODO: set this as the statement
+    var problemMeta = problemView.problemMeta;
+    var problemStats = problemMeta.problemStats;
+    //state.rating = content.metaData.rating; //TODO: This is causing an error
+    var tags = [];
+    var companies = [];
+    for(var i = 0; i < content.metaData.tagsList.length; ++i) {
+      var t = content.metaData.tagsList[i];
+      var parts = t.split("/");
+      // TODO: titlecase parts[1]
+      if(parts[0] === "topic") tags.push(parts[1]);
+      else companies.push(parts[1]);
+    }
+    state.tags = tags;
+    state.companies = companies;
+    state.customInputs = problemMeta.argsList.map(a => ({name: a.name, type: "int[]", inputType: "text"}));
+    state.accuracy = problemStats.accuracy; // TODO: Format as xx.yy%
+    state.difficulty = ["Easy", "Medium", "Hard"][problemStats.difficulty];
+    state.frequency = "Often";
   }
 };
 
